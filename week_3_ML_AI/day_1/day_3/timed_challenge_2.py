82d20ef8e3cd9dd073b015c493c4808d497d1212
mariedaviv <mariekram@gmail.com>
2026-03-10 20:19:50 +0200

    change

--- week_3_ML_AI/day_1/day_3/timed_challenge_2.py
+++
@@ -1,3 +0,0 @@
-x = int(input('Enter the Number:')) 
-divisors = [num for num in range(1,x) if x%num==0]
-print(sum(divisors) == x)
--- week_3_ML_AI/day_1/day_4/timed_challenge_1.py
+++
@@ -1,8 +0,0 @@
-word = input("Write something: " ).lower()
-char = input('Write a character: ').lower()
-
-def num_letter(word,char):
-    print(word.count(char))
-
-
-num_letter(word,char)
